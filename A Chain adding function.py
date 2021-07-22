d4dfc2698a3cc3e5bcd527b55343e9a9edb06d59
gumaonelove <68272600+gumaonelove@users.noreply.github.com>
2021-07-22 13:49:38 +0300

    first commit

--- A Chain adding function.py
+++ A Chain adding function.py
@@ -1,12 +1,3 @@
-result = 0
-
-def add(*args):
-    global result
-    for arg in args:
-        result += arg
-    return result
-
-add(1)
-add(2)
-add(3)
-print(add(4))
+class add(int):
+    def __call__(self, n):
+        return add(self + n)
